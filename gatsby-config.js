39782bd5b37d9b7cfa1e959084eaf4b004170c2d
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-08-12 16:30:41 +0100

    Added styled components.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Ezeikel Pemberton - Front End Developer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
