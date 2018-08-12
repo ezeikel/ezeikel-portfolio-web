f663aff2ea08a5961bde336a2f2bcb1c771df1c1
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-08-12 16:50:56 +0100

    Header is using styled components.

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from "styled-components";
+
+const Wrapper = styled.header`
+  display: grid;
+  background-color: tomato;
+  padding: 32px;
+  font-size: 32px;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+  <Wrapper>
+    <StyledLink to="/">{siteTitle}</StyledLink>
+  </Wrapper>
+);
 
 export default Header
