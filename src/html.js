1a01455611198f28e0c5346fb71062e480697860
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-08-29 19:15:19 +0100

    The correct GA tag.

--- src/html.js
+++ src/html.js
@@ -43,27 +43,15 @@ module.exports = class HTML extends React.Component {
               `
             }}
           />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-60859122-2"></script>
           <script
             dangerouslySetInnerHTML={{
               __html:`
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                ga('create', 'UA-60859122-1', 'auto');
-                ga('send', 'pageview');
-              `
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html:`
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                ga('create', 'UA-60859122-1', 'auto');
-                ga('send', 'pageview');
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-60859122-2');
               `
             }}
           />
